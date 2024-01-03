aa169811e16a8fd410306a87deaebb7f9f1c8bad
Corentin <ccouasno@gmail.com>
2024-01-03 16:02:46 +0100

    Added FFA Game mode

---
+++ Assets/FPS/Scripts/Game/Shared/GameMode.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Unity.FPS.Game
+{
+    public class GameMode : MonoBehaviour
+    {
+        protected virtual void OnEnable()
+        {
+            EventManager.AddListener<AllObjectivesCompletedEvent>(OnAllObjectivesCompleted);
+        }
+
+        protected virtual void OnDisable()
+        {
+            EventManager.RemoveListener<AllObjectivesCompletedEvent>(OnAllObjectivesCompleted);
+        }
+
+        protected virtual void OnAllObjectivesCompleted(AllObjectivesCompletedEvent evt) { }
+    }
+}
